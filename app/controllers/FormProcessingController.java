313d19ae7b5db8be2af1305b4197bb7c5ba78d23
Raphaël Marbeck <raphael@hometime.fr>
2020-03-31 18:41:10 +0200

    Flattening values

--- app/controllers/FormProcessingController.java
+++ app/controllers/FormProcessingController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.stream.Collectors;
@@ -49,23 +50,21 @@ public class FormProcessingController extends Controller implements WSBodyReadab
 	public CompletionStage<Result> processQuotationRequest(Http.Request request) {
 		final Form<QuotationRequestData> boundForm = formFactory.form(QuotationRequestData.class).withDirectFieldAccess(true).bindFromRequest(request);
 		
-		logger.error(" iiiiiiiiiiiiiii  => "+boundForm.get().brand);
-		logger.error(" iiiiiiiiiiiiiii  => "+boundForm.get().city);
-		
-		logger.error(" oooooooooooooo  => "+request.body().asFormUrlEncoded());
-		logger.error(" oooooooooooooo  => "+request.body().asFormUrlEncoded().entrySet().stream().map(entry -> entry.getKey()+"="+entry.getValue()[0]).collect( Collectors.joining( "&" )));
-		
 		if (boundForm.hasErrors()) {
 			logger.error("######Error#######");
 			return CompletableFuture.supplyAsync(() -> badRequest(views.html.quotation_form.render(boundForm, request, messagesApi.preferred(request))));
 		} else {
-			CompletionStage<? extends WSResponse> responsePromise = ws.url("https://www.hometime.fr/new-order-from-outside").setContentType("application/x-www-form-urlencoded").post(request.body().asFormUrlEncoded().entrySet().stream().map(entry -> entry.getKey()+"="+entry.getValue()[0]).collect( Collectors.joining( "&" )));
+			CompletionStage<? extends WSResponse> responsePromise = ws.url("https://www.hometime.fr/new-order-from-outside").setContentType("application/x-www-form-urlencoded").post(request.body().asFormUrlEncoded().entrySet().stream().map(entry -> flattenValues(entry.getKey(), entry.getValue(), "&")).collect( Collectors.joining( "&" )));
 			return responsePromise.thenApply( response -> {
 				logger.error("!!!!!!!!!!!!!!!!!!!!! -> "+response.getStatus());
 				return displayFormContent(request, boundForm.get().brand);
 			});
 			
 		}
+	}
+	
+	private String flattenValues(String key, String[] values, String separator) {
+		return Arrays.asList(values).stream().map(value -> key+"="+value).collect(Collectors.joining( "&" ));
 	}
 
 }
