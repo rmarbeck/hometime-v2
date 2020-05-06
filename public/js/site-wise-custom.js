9402086e32126de40c4bc8df71badd180e82d40f
Raphaël Marbeck <raphael@hometime.fr>
2020-05-06 22:08:38 +0200

    Add safari support for webp alternatives

--- public/js/site-wise-custom.js
+++ public/js/site-wise-custom.js
@@ -19,3 +19,28 @@ video_wrapper.addClass("embed-responsive-16by9");
 video_wrapper.html('<iframe style="border:none;"  allowfullscreen class="embed-responsive-item" src="' + video_wrapper.data('yt-url') + '"></iframe>');
 });
 }
+
+async function supportsWebp() {
+  if (!self.createImageBitmap) return false;
+  
+  const webpData = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=';
+  const blob = await fetch(webpData).then(r => r.blob());
+  return createImageBitmap(blob).then(() => true, () => false);
+}
+
+(async () => {
+  if(await supportsWebp()) {
+    console.log('webp supported');
+  }
+  else {
+    $('.webp-alt-video > video').each(function() {
+    	var replacePoster = $(this).attr("data-alt-poster");
+    	$(this).attr("poster", replacePoster);
+    });
+    
+    $('.webp-alt-style').each(function() {
+    	var replaceStyle = $(this).attr("data-alt-style");
+    	$(this).attr("style", replaceStyle);
+    });
+  }
+})();
