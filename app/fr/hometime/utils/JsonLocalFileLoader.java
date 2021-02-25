7fe0c932ae40dcdc23dba9c15e92fb3435feace1
rmarbeck <raphael@hometime.fr>
2021-02-25 12:32:27 +0100

    Testing files on production

--- app/fr/hometime/utils/JsonLocalFileLoader.java
+++ app/fr/hometime/utils/JsonLocalFileLoader.java
@@ -5,11 +5,32 @@ import java.util.Optional;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import play.Logger;
 import play.api.Play;
 import play.libs.Json;
 
 public interface JsonLocalFileLoader {
 	default Optional<JsonNode> loadJson(String filePath) throws Exception {
+		String[] pathnames;
+		File f = new File("/");
+		pathnames = f.list();
+		for (String pathname : pathnames) {
+			Logger.error(pathname);
+        }
+		
+		f = new File("/conf/");
+		pathnames = f.list();
+		for (String pathname : pathnames) {
+			Logger.error(pathname);
+        }
+		
+		f = new File("conf/");
+		pathnames = f.list();
+		for (String pathname : pathnames) {
+			Logger.error(pathname);
+        }
+		
+		
 		JsonNode json = Json.mapper().readTree(new File(filePath));
 		return Optional.of(json);
 	}
